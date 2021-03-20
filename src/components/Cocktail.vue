6a22c4787a749d29c94f1edd5b1ba0aaa8e2d207
Simone <simone.bervi@gmail.com>
2021-03-20 19:54:16 +0100

    Fix condizione if per il controllo delle key degli ingredienti

--- src/components/Cocktail.vue
+++ src/components/Cocktail.vue
@@ -1,19 +1,21 @@
 <template>
   <div>
     <div class="cocktail_info" v-for="(cocktail,index) in product" :key="index">
-      <img :src="cocktail.strDrinkThumb" alt="cocktail-img" width="500px" height="500px">
+      <img class="cocktail_image" :src="cocktail.strDrinkThumb" alt="cocktail-img" width="700px" height="700px">
       <div class="info_content">
+        <h3>Home / {{cocktail.strDrink}}</h3>
         <h1>{{cocktail.strDrink}}</h1>
         <h2>Instruction:</h2>
         <p>{{cocktail.strInstructions}}</p>
         <h2>Glass:</h2>
-        <p>{{cocktail.strGlass}}</p>
+        <p>Serve: {{cocktail.strGlass}}</p>
       </div>
     </div>
 
+    <h2>Ingredients ({{ingredients.length}})</h2>
     <div class="cocktail_ingredients" v-for="(ingredient,index) in ingredients" :key="index">
       <div v-if="ingredient != null">
-        <img :src="'https://www.thecocktaildb.com/images/ingredients/' + ingredient + '-Medium.png'" alt="cocktail-img" width="500px" height="500px">
+        <img :src="'https://www.thecocktaildb.com/images/ingredients/' + ingredient + '-Medium.png'" alt="cocktail-img">
         <h3>{{ingredient}}</h3>
       </div>
     </div>
@@ -35,7 +37,7 @@ export default {
     var self = this;
     var key;
 
-    // mi creo un array contententi le i nomi delle chiavi riferite agli ingredienti
+    // mi creo un array contententi i nomi delle chiavi riferite agli ingredienti
     for (let i = 1; i <= 15; i++) {
       self.ingredientKey.push('strIngredient' + i);
     }
@@ -48,7 +50,7 @@ export default {
           // vado a ciclare nell'oggetto contentente tutte le informazioni del drink andando a confrontare se le chiavi dell'oggetto hanno una corrispondenza con i nomi delle chiavi degli ingredienti del drink, se trovo una corrispondenza pusho il valore della chiave in un array 'ingredients' che mi servirà per andare a creare le card contenenti gli ingredienti.
           for (key in self.product[0]) {
             var ingredient = self.ingredientKey.includes(key);
-            if (ingredient) {
+            if (ingredient && self.product[0][key] != null) {
               self.ingredients.push(self.product[0][key]);
             }
           }
@@ -61,6 +63,27 @@ export default {
 <style scoped>
   .cocktail_info {
     display: flex;
+    align-items: center;
+    width: 65%;
+    margin: auto;
     margin-top: 5%;
+  }
+  .cocktail_image {
+    border-radius: 15px;
+  }
+  .info_content {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    max-width: 400px;
+    height: 700px;
+    margin-left: 10%;
+  }
+  .info_content h1 {
+    margin: 8% 0;
+    font-size: 50px;
+  }
+  .info_content h2 {
+    margin: 5% 0;
   }
 </style>
