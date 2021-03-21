d45f915629d0245703875c4f5f8ba8f71b7eea16
Simone <simone.bervi@gmail.com>
2021-03-21 09:48:34 +0100

    Aggiunte quantità dei corrispettivi ingredienti

--- src/components/Cocktail.vue
+++ src/components/Cocktail.vue
@@ -12,12 +12,16 @@
       </div>
     </div>
 
-    <h2>Ingredients ({{ingredients.length}})</h2>
-    <div class="cocktail_ingredients" v-for="(ingredient,index) in ingredients" :key="index">
-      <div v-if="ingredient != null">
-        <img :src="'https://www.thecocktaildb.com/images/ingredients/' + ingredient + '-Medium.png'" alt="cocktail-img">
-        <h3>{{ingredient}}</h3>
+    <div class="cocktail_ingredients_wrapper">
+      <h2>Ingredients ({{ingredients.length}})</h2>
+      <div class="cocktail_ingredients_card_container">
+        <div class="cocktail_ingredients_card" v-for="(ingredient,index) in ingredients" :key="index">
+        <div class="cocktail_ingredients_card_content" v-if="ingredient != null">
+          <img :src="'https://www.thecocktaildb.com/images/ingredients/' + ingredient + '-Medium.png'" alt="cocktail-img" width="200px" height="200px">
+          <h3>{{ measures[index] }} {{ ingredient }}</h3>
+        </div>
       </div>
+    </div>
     </div>
   </div>
 </template>
@@ -30,16 +34,20 @@ export default {
     return {
       product: [],
       ingredientKey: [],
+      measureKey: [],
       ingredients: [],
+      measures: [],
     }
   },
   mounted: function () {
     var self = this;
     var key;
+    
 
-    // mi creo un array contententi i nomi delle chiavi riferite agli ingredienti
+    // vado a pushare in due array contententi i nomi delle chiavi riferite agli ingredienti
     for (let i = 1; i <= 15; i++) {
       self.ingredientKey.push('strIngredient' + i);
+      self.measureKey.push('strMeasure' + i);
     }
 
     axios
@@ -50,8 +58,14 @@ export default {
           // vado a ciclare nell'oggetto contentente tutte le informazioni del drink andando a confrontare se le chiavi dell'oggetto hanno una corrispondenza con i nomi delle chiavi degli ingredienti del drink, se trovo una corrispondenza pusho il valore della chiave in un array 'ingredients' che mi servirà per andare a creare le card contenenti gli ingredienti.
           for (key in self.product[0]) {
             var ingredient = self.ingredientKey.includes(key);
+            var measure = self.measureKey.includes(key);
+            
             if (ingredient && self.product[0][key] != null) {
               self.ingredients.push(self.product[0][key]);
+            }
+
+            if (measure && self.product[0][key] != null) {
+              self.measures.push(self.product[0][key]);
             }
           }
       });
@@ -85,5 +99,44 @@ export default {
   }
   .info_content h2 {
     margin: 5% 0;
+  }
+  .cocktail_ingredients_wrapper {
+    width: 65%;
+    margin: auto;
+    margin-top: 5%;
+  }
+  .cocktail_ingredients_wrapper > h2 {
+    text-align: center;
+  }
+  .cocktail_ingredients_card_container {
+    display: flex;
+    flex-wrap: wrap;
+    width: 100%;
+    margin-top: 2%;
+  }
+  .cocktail_ingredients_card {
+    width: 350px;
+    height: 350px;
+    margin: 20px 10px;
+    border-top-left-radius: 100px;
+    border-top-right-radius: 50px;
+    border-bottom-right-radius: 100px;
+    box-shadow: 4px 4px 15px 1px rgba(0, 0, 0, 0.5);
+    background-color: #F5F5F5;
+    overflow: hidden;
+    cursor: pointer;
+    transition: 1s;
+  }
+  .cocktail_ingredients_card:hover {
+    transform: scale(1.025);
+  }
+  .cocktail_ingredients_card_content {
+    width: 100%;
+    height: 100%;
+    padding: 10% 0;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
   }
 </style>
